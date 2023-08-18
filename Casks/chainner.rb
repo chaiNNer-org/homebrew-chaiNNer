2f10bb899b9ea576c64559741d76fc7060157f2b
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-08-18 08:17:59 +0000

    chainner 0.19.3

--- Casks/chainner.rb
+++ Casks/chainner.rb
@@ -1,7 +1,7 @@
 cask "chainner" do
-  version "0.19.2"
+  version "0.19.3"
   # TODO: remove sh256 and url when an arm64 build is available
-  sha256 "6445be7f64973195045fc27d4cd894b5a8a162bf7428019e76ec1f9c93b8e6f3"
+  sha256 "7479e5d321d6737c094fac3fc47ab719ec239232abd5298e64d6a98bed121304"
 
   url "https://github.com/chaiNNer-org/chaiNNer/releases/download/v#{version}/chaiNNer-#{version}-x64-macos.dmg",
       verified: "github.com/chaiNNer-org/chaiNNer/"
@@ -13,7 +13,7 @@ cask "chainner" do
   #         verified: "github.com/chaiNNer-org/chaiNNer/"
   #   end
   #   on_intel do
-  #     sha256 "6445be7f64973195045fc27d4cd894b5a8a162bf7428019e76ec1f9c93b8e6f3"
+  #     sha256 "7479e5d321d6737c094fac3fc47ab719ec239232abd5298e64d6a98bed121304"
   #
   #     url "https://github.com/chaiNNer-org/chaiNNer/releases/download/v#{version}/chaiNNer-#{version}-x64-macos.dmg",
   #         verified: "github.com/chaiNNer-org/chaiNNer/"
